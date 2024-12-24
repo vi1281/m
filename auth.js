0e2612b7712feebecf7a656eb178e1c65680fff9
vi1281 <vika30072004@gmail.com>
2024-12-24 10:42:04 +0100

    моя бд

--- auth.js
+++ auth.js
@@ -1,25 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDRhYwxR2O1zeQFZQf_A70Jf4zNESlBfPc",
-  authDomain: "dbase-a9d5a.firebaseapp.com",
-  projectId: "dbase-a9d5a",
-  storageBucket: "dbase-a9d5a.firebasestorage.app",
-  messagingSenderId: "16367447538",
-  appId: "1:16367447538:web:0eb42e275dbb4d2860cf6b"
+  apiKey: "AIzaSyDD7wYR16lWYvHN6FlvZu9UpLzXW8dw5Mo",
+  authDomain: "login-72844.firebaseapp.com",
+  projectId: "login-72844",
+  storageBucket: "login-72844.firebasestorage.app",
+  messagingSenderId: "856995387716",
+  appId: "1:856995387716:web:00a49160d4a84e08c29f11"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Get buttons
+
 const loginButton = document.getElementById('login');
 const registerButton = document.getElementById('register');
 
-// Event listener for login
 loginButton.addEventListener("click", function (event) {
   event.preventDefault();
 
@@ -34,9 +31,7 @@ loginButton.addEventListener("click", function (event) {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-      // Save email to localStorage for home page
       localStorage.setItem("userEmail", user.email);
-      // Redirect to home page
       window.location.href = "home.html";
     })
     .catch((error) => {
