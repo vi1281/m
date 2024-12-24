8920cd55d82a9e6e66b82a1b943ff860de967b6a
vi1281 <vika30072004@gmail.com>
2024-12-24 12:50:01 +0100

    мяоя бд

--- auth.js
+++ auth.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyDRhYwxR2O1zeQFZQf_A70Jf4zNESlBfPc",
-  authDomain: "dbase-a9d5a.firebaseapp.com",
-  projectId: "dbase-a9d5a",
-  storageBucket: "dbase-a9d5a.firebasestorage.app",
-  messagingSenderId: "16367447538",
-  appId: "1:16367447538:web:0eb42e275dbb4d2860cf6b"
-};
-
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDD7wYR16lWYvHN6FlvZu9UpLzXW8dw5Mo",
+    authDomain: "login-72844.firebaseapp.com",
+    projectId: "login-72844",
+    storageBucket: "login-72844.firebasestorage.app",
+    messagingSenderId: "856995387716",
+    appId: "1:856995387716:web:00a49160d4a84e08c29f11"
+  };
+  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
